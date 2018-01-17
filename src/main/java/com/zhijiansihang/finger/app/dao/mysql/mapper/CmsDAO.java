37d1f295915acc975c54a220937419ee550fecdf
paul <1003681427@qq.com>
2018-01-17 21:35:33 +0800

    初始化

--- src/main/java/com/zhijiansihang/finger/app/dao/mysql/mapper/CmsDAO.java
+++ src/main/java/com/zhijiansihang/finger/app/dao/mysql/mapper/CmsDAO.java
@@ -4,6 +4,7 @@ import com.zhijiansihang.finger.app.dao.mysql.model.CmsDO;
 import com.zhijiansihang.finger.app.dao.mysql.model.CmsDOExample;
 import java.util.List;
 
+import com.zhijiansihang.finger.app.vo.CmsVO;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.session.RowBounds;
 @Mapper
@@ -106,4 +107,25 @@ public interface CmsDAO {
         "where id = #{id,jdbcType=BIGINT}"
     })
     int updateByPrimaryKey(CmsDO record);
+
+
+    @Select({
+            "select count(*)",
+            "from cms",
+            "where type_code = #{typeCode}",
+    })
+    int countByCmsVO(CmsVO cmsVO);
+
+    @Select({
+            "select",
+            "id, type_code, type_name, image_path, image_access_path, title, sub_title, content, ",
+            "href_link, is_front_display, is_deleted, location_size, create_time, create_by, ",
+            "update_time, update_by",
+            "from cms",
+            "where type_code = #{typeCode}",
+            "order by id desc"
+    })
+    @ResultMap("com.zhijiansihang.finger.app.dao.mysql.mapper.CmsDAO.BaseResultMap")
+    List<CmsDO> selectByCmsVO(CmsVO cmsVO, RowBounds rowBounds);
+
 }
