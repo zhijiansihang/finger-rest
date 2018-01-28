a6327f2ee222843a80920d2b3728d185e3db917b
paul <1003681427@qq.com>
2018-01-28 18:13:47 +0800

    方案详情

--- src/main/java/com/zhijiansihang/finger/gen/serviceImpl/SolutionDetailService.java
+++ src/main/java/com/zhijiansihang/finger/gen/serviceImpl/SolutionDetailService.java
@@ -1,6 +1,8 @@
 package com.zhijiansihang.finger.gen.serviceImpl;
 
 
+import com.zhijiansihang.finger.app.dao.mysql.mapper.UserSolutionDAO;
+import com.zhijiansihang.finger.app.dao.mysql.model.UserSolutionDO;
 import com.zhijiansihang.finger.mmc.MessageService;
 import com.zhijiansihang.common.Response;
 import com.zhijiansihang.finger.gen.entity.SolutionDetailRequest;
@@ -8,6 +10,7 @@ import com.zhijiansihang.finger.gen.entity.SolutionDetailResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,17 +23,18 @@ public class SolutionDetailService implements MessageService<SolutionDetailReque
 	private static final Logger LOG = LoggerFactory.getLogger(SolutionDetailService.class);
 	private static final String SERVICE_DESC = "方案详情";
 
+	@Autowired
+	UserSolutionDAO userSolutionDAO;
 	@Override
 	public void execute(SolutionDetailRequest request, Response<SolutionDetailResponse> response) {
 		LOG.info("[{}][request={}]", SERVICE_DESC, request);
-
-		response.getBody().setEarningType("1");
-		response.getBody().setExpectedDeadline("1");
+		String solutionId = request.getSolutionId();
+		UserSolutionDO userSolutionDO = userSolutionDAO.selectByPrimaryKey(Long.parseLong(solutionId));
+		response.getBody().setEarningType(userSolutionDO.getEarningType().toString());
+		response.getBody().setExpectedDeadline(userSolutionDO.getExpectedDeadline().toString());
 		response.getBody().setIsFinanceDeleted("0");
-		response.getBody().setMoneySituation("1");
-		response.getBody().setRiskAssessmentLevel("10");
-		response.getBody().setSolution("10");
-	  	//挡板服务标志，实现该服务时，不要给mode赋值了，把下边的代码删了
-		response.getBody().setMode("test");
+		response.getBody().setMoneySituation(userSolutionDO.getMoneySituation().toString());
+		response.getBody().setRiskAssessmentLevel(userSolutionDO.getRiskAssessmentLevel().toString());
+		response.getBody().setSolution(userSolutionDO.getSolution());
 	}
 }
