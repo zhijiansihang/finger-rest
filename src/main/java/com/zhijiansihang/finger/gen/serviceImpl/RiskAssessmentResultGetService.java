0631de3bdb7206fdbcbd8af66c6cfcb594ad48bb
paul <1003681427@qq.com>
2018-03-05 21:34:13 +0800

    分险评测等级

--- src/main/java/com/zhijiansihang/finger/gen/serviceImpl/RiskAssessmentResultGetService.java
+++ src/main/java/com/zhijiansihang/finger/gen/serviceImpl/RiskAssessmentResultGetService.java
@@ -2,11 +2,15 @@ package com.zhijiansihang.finger.gen.serviceImpl;
 
 
 import com.zhijiansihang.common.Response;
+import com.zhijiansihang.finger.app.dao.mysql.mapper.UserSurverDAO;
+import com.zhijiansihang.finger.app.dao.mysql.model.UserSurverDO;
 import com.zhijiansihang.finger.gen.entity.RiskAssessmentResultGetRequest;
 import com.zhijiansihang.finger.gen.entity.RiskAssessmentResultGetResponse;
+import com.zhijiansihang.finger.gen.tool.UserTools;
 import com.zhijiansihang.finger.mmc.MessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,13 +23,19 @@ public class RiskAssessmentResultGetService implements MessageService<RiskAssess
 	private static final Logger LOG = LoggerFactory.getLogger(RiskAssessmentResultGetService.class);
 	private static final String SERVICE_DESC = "获取分险评测结果和答案";
 
+
+	@Autowired
+	UserSurverDAO userSurverDAO;
 	@Override
 	public void execute(RiskAssessmentResultGetRequest request, Response<RiskAssessmentResultGetResponse> response) {
 		LOG.info("[{}][request={}]", SERVICE_DESC, request);
 
-		response.getBody().setAnswers("{}");
-		response.getBody().setResult("1");
-	  	//挡板服务标志，实现该服务时，不要给mode赋值了，把下边的代码删了
-		response.getBody().setMode("test");
+		UserSurverDO userSurverDO = userSurverDAO.selectByPrimaryKey(UserTools.getLoginUser().getId());
+		if (userSurverDO !=null){
+			response.getBody().setAnswers(userSurverDO.getAnswers());
+			response.getBody().setResult(userSurverDO.getRiskAssessmentLevel().toString());
+		}
+
+
 	}
 }
--- src/main/java/com/zhijiansihang/finger/gen/serviceImpl/RiskAssessmentResultPostService.java
+++ src/main/java/com/zhijiansihang/finger/gen/serviceImpl/RiskAssessmentResultPostService.java
@@ -2,12 +2,28 @@ package com.zhijiansihang.finger.gen.serviceImpl;
 
 
 import com.zhijiansihang.common.Response;
+import com.zhijiansihang.common.ResponseHeader;
+import com.zhijiansihang.common.ResponseHeaderBuilder;
+import com.zhijiansihang.finger.app.constant.CmsConsts;
+import com.zhijiansihang.finger.app.dao.mysql.mapper.UserDAO;
+import com.zhijiansihang.finger.app.dao.mysql.mapper.UserSurverDAO;
+import com.zhijiansihang.finger.app.dao.mysql.model.UserDO;
+import com.zhijiansihang.finger.app.dao.mysql.model.UserSurverDO;
+import com.zhijiansihang.finger.app.event.demo.RiskAssessmentModifyEvent;
+import com.zhijiansihang.finger.app.sharing.spring.ApplicationContextHelper;
 import com.zhijiansihang.finger.gen.entity.RiskAssessmentResultPostRequest;
 import com.zhijiansihang.finger.gen.entity.RiskAssessmentResultPostResponse;
+import com.zhijiansihang.finger.gen.tool.CheckTools;
+import com.zhijiansihang.finger.gen.tool.UserTools;
 import com.zhijiansihang.finger.mmc.MessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static com.zhijiansihang.finger.app.constant.CmsConsts.getRiskAssessment;
 
 /**
  * 分险评测结果
@@ -19,11 +35,56 @@ public class RiskAssessmentResultPostService implements MessageService<RiskAsses
 	private static final Logger LOG = LoggerFactory.getLogger(RiskAssessmentResultPostService.class);
 	private static final String SERVICE_DESC = "分险评测结果";
 
+
+	@Autowired
+	UserSurverDAO userSurverDAO;
+
+	@Autowired
+	UserDAO userDAO;
 	@Override
 	public void execute(RiskAssessmentResultPostRequest request, Response<RiskAssessmentResultPostResponse> response) {
 		LOG.info("[{}][request={}]", SERVICE_DESC, request);
 
-	  	//挡板服务标志，实现该服务时，不要给mode赋值了，把下边的代码删了
-		response.getBody().setMode("test");
+		Long userid = UserTools.getLoginUser().getId();
+
+		String answers = request.getAnswers();
+		String result = request.getResult();
+		if (answers==null || !CheckTools.isInteger(result)){
+			ResponseHeader responseHeader = ResponseHeaderBuilder.buildValidateError("参数不能为空");
+			response.setHeader(responseHeader);
+			return;
+		}
+
+		String riskAssessment = getRiskAssessment(Integer.parseInt(result));
+		if (riskAssessment == null || riskAssessment.length() == 0){
+			ResponseHeader responseHeader = ResponseHeaderBuilder.buildValidateError("riskAssessment参数有误");
+			response.setHeader(responseHeader);
+			return;
+		}
+
+		UserSurverDO userSurverDO = new UserSurverDO();
+		userSurverDO.setAnswers(answers);
+		userSurverDO.setCreateTime(new Date());
+		userSurverDO.setRiskAssessmentLevel(Short.parseShort(result));
+		userSurverDO.setUserId(userid);
+		userSurverDAO.insert(userSurverDO);
+
+		UserDO userDO1 = userDAO.selectByPrimaryKey(userid);
+		if (userDO1.getRiskAssessmentLevel() != null &&Short.parseShort(result) == userDO1.getRiskAssessmentLevel().shortValue())
+		{
+			LOG.info("用户{}分险测评和上一次结果[{}]相同,不触发跑批",userid,userDO1.getRiskAssessmentLevel());
+			return;
+		}
+
+		UserDO userDO = new UserDO();
+		userDO.setUserId(userid);
+		userDO.setRiskAssessmentLevel(Short.parseShort(result));
+		userDAO.updateByPrimaryKeySelective(userDO);
+
+		RiskAssessmentModifyEvent riskAssessmentModifyEvent = new RiskAssessmentModifyEvent(this);
+		riskAssessmentModifyEvent.setUserId(userid);
+
+		ApplicationContextHelper.applicationContext.publishEvent(riskAssessmentModifyEvent);
+
 	}
 }
