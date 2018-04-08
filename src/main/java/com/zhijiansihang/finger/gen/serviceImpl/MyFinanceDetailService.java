0aad0921cfc39461fee9c47b9134cdd81252196f
paul <1003681427@qq.com>
2018-04-08 22:39:56 +0800

    修改接口

--- src/main/java/com/zhijiansihang/finger/gen/serviceImpl/MyFinanceDetailService.java
+++ src/main/java/com/zhijiansihang/finger/gen/serviceImpl/MyFinanceDetailService.java
@@ -23,39 +23,39 @@ import static com.zhijiansihang.common.RetCode.NOPERMISSION;
 
 /**
  * 我的资料
- * 
  */
 @Component("myFinanceDetailService")
 public class MyFinanceDetailService implements MessageService<MyFinanceDetailRequest, Response<MyFinanceDetailResponse>> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MyFinanceDetailService.class);
-	private static final String SERVICE_DESC = "我的资料";
+    private static final Logger LOG = LoggerFactory.getLogger(MyFinanceDetailService.class);
+    private static final String SERVICE_DESC = "我的资料";
 
-	@Autowired
-	UserFinanceDetailDAO userFinanceDetailDAO;
-	@Autowired
-	UserDAO userDAO;
-	@Override
-	public void execute(MyFinanceDetailRequest request, Response<MyFinanceDetailResponse> response) {
-		LOG.info("[{}][request={}]", SERVICE_DESC, request);
+    @Autowired
+    UserFinanceDetailDAO userFinanceDetailDAO;
+    @Autowired
+    UserDAO userDAO;
 
-		Long userId = UserTools.getLoginUser().getId();
-		UserDO userDO = userDAO.selectByPrimaryKey(userId);
-		Short roles = userDO.getRoles();
-		if (!UserConsts.UserRolesEnum.FINANCE.name().equals(UserConsts.getUserRoles(roles.intValue()))){
-			ResponseHeader responseHeader = ResponseHeaderBuilder.build(NOPERMISSION,"只允许理财师可以操作");
-			response.setHeader(responseHeader);
-			return;
-		}
-		UserFinanceDetailDO userFinanceDetailDO = userFinanceDetailDAO.selectByPrimaryKey(userId);
-		response.getBody().setEducationLevel(userFinanceDetailDO.getEducationLevel());
-		response.getBody().setIsAdd(userFinanceDetailDO.getIsAdd()!=null?userFinanceDetailDO.getIsAdd().toString():"0");
-		response.getBody().setPersonalProfile(userFinanceDetailDO.getPersonalProfile());
-		response.getBody().setPosition(userFinanceDetailDO.getPosition());
-		response.getBody().setSchoolName(userFinanceDetailDO.getSchoolName());
-		response.getBody().setServiceConcept(userFinanceDetailDO.getServiceConcept());
-		response.getBody().setServiceDirection(userFinanceDetailDO.getServiceDirection());
-		response.getBody().setWorkAge(userFinanceDetailDO.getWorkAge().toString());
-		response.getBody().setWorkingExperience(userFinanceDetailDO.getWorkingExperience());
-	}
+    @Override
+    public void execute(MyFinanceDetailRequest request, Response<MyFinanceDetailResponse> response) {
+        LOG.info("[{}][request={}]", SERVICE_DESC, request);
+
+        Long userId = UserTools.getLoginUser().getId();
+        UserDO userDO = userDAO.selectByPrimaryKey(userId);
+        Short roles = userDO.getRoles();
+        if (!UserConsts.UserRolesEnum.FINANCE.name().equals(UserConsts.getUserRoles(roles.intValue()))) {
+            ResponseHeader responseHeader = ResponseHeaderBuilder.build(NOPERMISSION, "只允许理财师可以操作");
+            response.setHeader(responseHeader);
+            return;
+        }
+        UserFinanceDetailDO userFinanceDetailDO = userFinanceDetailDAO.selectByPrimaryKey(userId);
+        response.getBody().setEducationLevel(userFinanceDetailDO.getEducationLevel());
+        response.getBody().setIsAdd(userFinanceDetailDO.getIsAdd() != null ? userFinanceDetailDO.getIsAdd().toString() : "0");
+        response.getBody().setPersonalProfile(userFinanceDetailDO.getPersonalProfile());
+        response.getBody().setPosition(userFinanceDetailDO.getPosition());
+        response.getBody().setSchoolName(userFinanceDetailDO.getSchoolName());
+        response.getBody().setServiceConcept(userFinanceDetailDO.getServiceConcept());
+        response.getBody().setServiceDirection(userFinanceDetailDO.getServiceDirection());
+        response.getBody().setWorkAge(userFinanceDetailDO.getWorkAge() == null ? "" : userFinanceDetailDO.getWorkAge().toString());
+        response.getBody().setWorkingExperience(userFinanceDetailDO.getWorkingExperience());
+    }
 }
